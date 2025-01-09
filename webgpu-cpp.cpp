6086735b9ba78e05f4805c5275a24b098b0a94a0
Pete Lewis <mygit@pjblewis.com>
2025-01-09 08:56:27 -0500

    Attempting to fix Linux exports (mostly failing)

--- webgpu-cpp.cpp
+++ webgpu-cpp.cpp
@@ -1,11 +1,12 @@
-#include <windows.h>
-
 #define WGPU_SHARED_LIBRARY
 #define WGPU_IMPLEMENTATION
 #define WEBGPU_CPP_IMPLEMENTATION
 #include <webgpu/webgpu.hpp>
 
+#ifdef WIN32
+#include <Windows.h>
 BOOL APIENTRY DllMain(HANDLE hModule, DWORD ulReasonForCall, LPVOID lpReserved)
 {
     return TRUE;
 }
+#endif // WIN32
